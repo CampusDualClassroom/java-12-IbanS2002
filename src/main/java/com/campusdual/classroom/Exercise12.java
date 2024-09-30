d12bb414bc6bb22c76c9cdfbee37a067d5cf2f5c
Ivan Silvosa Rodriguez <ivansrlsy@gmail.com>
2024-09-30 18:26:14 +0200

    Ejercicio Resuelto

--- src/main/java/com/campusdual/classroom/Exercise12.java
+++ src/main/java/com/campusdual/classroom/Exercise12.java
@@ -2,7 +2,44 @@ package com.campusdual.classroom;
 
 public class Exercise12 {
     public static void main(String[] args) {
+        Car coche= new Car("Citroen", "Xsara", "Diesel");
 
+        //coche.start();
+        //coche.accelerate();
+        //coche.stop();
+        //coche.showDetails();
+        coche.showDetails();
+
+        // Intentar encender el coche
+        coche.start();
+
+        // Acelerar el coche a 50 km/h
+        coche.accelerate();
+
+        // Intentar encender el coche cuando ya está encendido
+        coche.start();
+
+        // Frenar el coche
+        coche.brake();
+
+        // Intentar frenar por debajo de 0
+        coche.brake();
+
+        // Girar el volante 20 grados
+        coche.turnAngleOfWheels(20);
+
+        // Intentar girar el volante más de 45 grados
+        coche.turnAngleOfWheels(40);
+
+        // Intentar poner marcha atrás mientras el coche está en movimiento
+        coche.setReverse(true);
+
+        // Detener el coche y poner marcha atrás
+        coche.brake();
+        coche.setReverse(true);
+
+        // Mostrar detalles finales
+        coche.showDetails();
     }
 
 }
